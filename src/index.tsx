472638c1ea678f81364bd9dcdd128b48ed6a205d
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-20 12:50:43 +0200

    Adds hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,18 @@ import ReactDOM from 'react-dom';
 import { App } from './app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement: HTMLElement | null = document.getElementById('root');
 
-/* 
+ReactDOM.render(<App />, rootElement);
+
+if (module.hot) {
+  module.hot.accept('./app', async () => {
+    const { App: NextApp } = await import('./app');
+    ReactDOM.render(<NextApp />, rootElement);
+  });
+}
+
+/*
 If you want your app to work offline and load faster, you can change
 unregister() to register() below. Note this comes with some pitfalls.
 Learn more about service workers: http://bit.ly/CRA-PWA
