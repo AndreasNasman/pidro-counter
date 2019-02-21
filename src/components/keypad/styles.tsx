cd9f1736388109a1abb837c1854b525006a7f930
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-21 17:53:22 +0200

    Adds poker table felt placeholder styled buttons

--- src/components/keypad/styles.tsx
+++ src/components/keypad/styles.tsx
@@ -3,19 +3,24 @@ import styled, { StyledComponent } from 'styled-components';
 const borderWidth: string = '1rem';
 
 export const Button: StyledComponent<'button', {}> = styled.button`
-  background-color: whitesmoke;
-  border-bottom: ${borderWidth} solid hsl(0, 0%, 80%);
-  border-left: ${borderWidth} solid hsl(0, 0%, 88%);
-  border-right: ${borderWidth} solid hsl(0, 0%, 88%);
-  border-top: ${borderWidth} solid hsl(0, 0%, 84%);
-  font-family: 'Roboto', sans-serif;
-  font-size: 2rem;
-  font-weight: 300;
-  padding: 0.75rem;
+  background-color: transparent;
+  border: 5px solid white;
+  border-radius: 10px;
+  color: white;
+  font: normal 2rem 'Roboto', sans-serif;
+  padding: 0.5rem;
+
+  &:focus {
+    background-color: white;
+    color: red;
+    font-weight: bold;
+    outline: none;
+  }
 `;
 
 export const Grid: StyledComponent<'div', {}> = styled.div`
   display: grid;
   grid-gap: 1px;
   grid-template-columns: 1fr 1fr 1fr;
+  padding: 1px;
 `;
