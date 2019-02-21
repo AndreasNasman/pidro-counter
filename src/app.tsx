0aa8c34a0f99560a0883285dff8b563207329eb2
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-21 16:39:27 +0200

    Adds global styling and container grid

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,32 @@
 import React, { FunctionComponent, ReactElement } from 'react';
-import styled, { StyledComponent } from 'styled-components';
+import styled, {
+  createGlobalStyle,
+  DefaultTheme,
+  GlobalStyleComponent,
+  StyledComponent,
+} from 'styled-components';
+import { Keypad } from './components/keypad/keypad';
 
-const Button: StyledComponent<'button', {}> = styled.button`
-  background-color: red;
-  color: white;
+const GlobalStyle: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
+  html,
+  body,
+  #root {
+    height: 100%;
+    }
+`;
+
+const Grid: StyledComponent<'div', {}> = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+  height: 100%;
 `;
 
 export const App: FunctionComponent = (): ReactElement => (
   <>
-    <div>Hello World!</div>
-    <Button>Click me!</Button>
+    <GlobalStyle />
+    <Grid>
+      <div>Hello World!</div>
+      <Keypad />
+    </Grid>
   </>
 );
