404ad62ec5e098a1bd24672919199d0236da858e
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-22 17:25:41 +0200

    Fullscreen changed to use fixed positioning.
    This takes into consideration the URL bar toggling on mobile.
    The application should now always stay fullscreen.

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,22 @@
 import React, { FunctionComponent, ReactElement } from 'react';
-import styled, {
-  createGlobalStyle,
-  DefaultTheme,
-  GlobalStyleComponent,
-  StyledComponent,
-} from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 import { Keypad } from './components/keypad/keypad';
 import { FELT_GREEN } from './constants/colors';
-
-const GlobalStyle: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
-  html,
-  body,
-  #root {
-    height: 100%;
-    }
-`;
 
 const Felt: StyledComponent<'div', {}> = styled.div`
   background-color: ${FELT_GREEN};
   box-shadow: inset 0 0 10rem black;
   display: grid;
   grid-template-rows: 1fr auto;
-  min-height: 100%;
+  height: 100%;
+  overflow: auto;
+  position: fixed;
+  width: 100%;
 `;
 
 export const App: FunctionComponent = (): ReactElement => (
-  <>
-    <GlobalStyle />
-    <Felt>
-      <div />
-      <Keypad />
-    </Felt>
-  </>
+  <Felt>
+    <div />
+    <Keypad />
+  </Felt>
 );
