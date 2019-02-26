67e7ff59297230b490f01273dda3cc7923e6bbc3
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-26 19:15:29 +0200

    Adds portrait spacing

--- src/app.tsx
+++ src/app.tsx
@@ -14,20 +14,19 @@ const GlobalStyle: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
     }
 `;
 
-const landscapeSpacing: number = 1;
+const spacing: number = 1;
 const Felt: StyledComponent<'div', {}> = styled.div`
   background-color: ${FELT_GREEN};
   box-shadow: inset 0 0 10rem black;
   display: grid;
   grid-template-rows: minmax(0, 1fr) auto;
-  height: 100vh;
+  height: calc(100vh - ${spacing + spacing}rem);
+  padding: ${spacing}rem;
 
   @media screen and (orientation: landscape) {
-    grid-gap: ${landscapeSpacing}rem;
+    grid-gap: ${spacing}rem;
     grid-template-columns: minmax(min-content, 2fr) minmax(min-content, 1fr);
-    height: calc(100vh - ${landscapeSpacing + landscapeSpacing}rem);
     overflow: auto;
-    padding: ${landscapeSpacing}rem;
   }
 `;
 
