4fb723f34ed8b7aaf03f0bbb724cef17e7717311
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-26 20:11:24 +0200

    Team state moved to common parent

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,18 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent, ReactElement, useState } from 'react';
 import { Keypad } from '../keypad';
 import { Scoreboard } from '../scoreboard';
 import { Felt, GlobalStyle } from './styles';
 
-export const App: FunctionComponent = (): ReactElement => (
-  <>
-    <GlobalStyle />
-    <Felt>
-      <Scoreboard />
-      <Keypad />
-    </Felt>
-  </>
-);
+export const App: FunctionComponent = (): ReactElement => {
+  const [teams] = useState(['vi', 'de']);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Felt>
+        <Scoreboard teams={teams} />
+        <Keypad teams={teams} />
+      </Felt>
+    </>
+  );
+};
---
+++ src/components/keypad/types.ts
@@ -0,0 +1,3 @@
+export interface IProps {
+  teams: string[];
+}
