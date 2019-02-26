c2b26a94c6cac5cc02bc9ca1b79b86212d7dd107
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-02-26 19:24:16 +0200

    Corrects spacing

--- src/app.tsx
+++ src/app.tsx
@@ -19,13 +19,13 @@ const Felt: StyledComponent<'div', {}> = styled.div`
   background-color: ${FELT_GREEN};
   box-shadow: inset 0 0 10rem black;
   display: grid;
-  grid-template-rows: minmax(0, 1fr) auto;
+  grid-template: minmax(0, 1fr) auto / 1fr;
   height: calc(100vh - ${spacing + spacing}rem);
   padding: ${spacing}rem;
 
   @media screen and (orientation: landscape) {
     grid-gap: ${spacing}rem;
-    grid-template-columns: minmax(min-content, 2fr) minmax(min-content, 1fr);
+    grid-template: 1fr / minmax(min-content, 2fr) minmax(min-content, 1fr);
     overflow: auto;
   }
 `;
