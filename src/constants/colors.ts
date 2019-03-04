804fac433c7452a1ecf8c569a152dec21c515587
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-03-04 12:38:26 +0200

    Refactors playing card colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,5 @@
 export const BLACK_BOARD: string = '#282725';
 export const CHALK_WHITE: string = '#fefeff';
 export const FELT_GREEN: string = '#006e3b';
-export const PLAYING_CARD_BLACK: string = 'black';
-export const PLAYING_CARD_RED: string = '#e10030';
+export const PLAYING_CARD_BLACK: string = '#000';
+export const PLAYING_CARD_RED: string = '#ff534f';
