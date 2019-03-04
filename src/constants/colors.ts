355553c5ab5663e11d275e30bf05cd7853e14e1b
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-03-04 12:51:09 +0200

    Refactors playing card color and button gap

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,3 +3,4 @@ export const CHALK_WHITE: string = '#fefeff';
 export const FELT_GREEN: string = '#006e3b';
 export const PLAYING_CARD_BLACK: string = '#000';
 export const PLAYING_CARD_RED: string = '#ff534f';
+export const PLAYING_CARD_WHITE: string = '#fff';
