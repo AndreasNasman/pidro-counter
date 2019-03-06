e5a4aceba70d424464db539a05e615c8f41eca84
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-03-06 13:22:35 +0200

    Adds confirmation on game reset

--- src/components/toolbar/styles.ts
+++ src/components/toolbar/styles.ts
@@ -1,5 +1,24 @@
 import styled, { StyledComponent } from 'styled-components';
 import { CHALK_WHITE } from '../../constants/colors';
+import { IAnswer } from './types';
+
+export const Answer: StyledComponent<'span', {}, IAnswer> = styled.span`
+  color: ${(props: IAnswer): string => props.color};
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 0.5rem;
+`;
+
+export const ChoiceContainer: StyledComponent<'div', {}> = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Confirmation: StyledComponent<'div', {}> = styled.div`
+  background-color: white;
+  border-radius: 0.5rem;
+  padding: 1rem;
+`;
 
 export const Container: StyledComponent<'div', {}> = styled.div`
   color: ${CHALK_WHITE};
@@ -9,7 +28,12 @@ export const Container: StyledComponent<'div', {}> = styled.div`
   justify-content: space-between;
 `;
 
-export const IconWrapper: StyledComponent<'div', {}> = styled.div`
+export const H4: StyledComponent<'h4', {}> = styled.h4`
+  margin-top: 0;
+  text-align: center;
+`;
+
+export const IconWrapper: StyledComponent<'span', {}> = styled.span`
   align-items: center;
   display: flex;
   padding: 0.5rem;
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,7 @@
-export const BLACK_BOARD: string = '#282725';
+export const BLACK_BOARD: string = '#282828';
 export const CHALK_WHITE: string = '#fefeff';
+export const DECK_BLUE: string = '#12257d';
+export const DECK_RED: string = '#ea3f42';
 export const FELT_GREEN: string = '#006e3b';
 export const PLAYING_CARD_BLACK: string = '#000';
 export const PLAYING_CARD_RED: string = '#ff534f';
