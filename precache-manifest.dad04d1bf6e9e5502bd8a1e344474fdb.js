94e1411e4f971ef9546357a403e93d19691a11b8
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-03-20 16:41:52 +0200

    Updates

--- precache-manifest.dad04d1bf6e9e5502bd8a1e344474fdb.js
+++ precache-manifest.dad04d1bf6e9e5502bd8a1e344474fdb.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "9ee17be3e7434b97e009",
-    "url": "/pidro-counter/static/js/main.9ee17be3.chunk.js"
+    "revision": "ee38cf515b199efa890f",
+    "url": "/pidro-counter/static/js/main.ee38cf51.chunk.js"
   },
   {
     "revision": "7df57f6ad20a58c6bafb",
@@ -40,12 +40,12 @@ self.__precacheManifest = [
     "url": "/pidro-counter/static/media/roboto-latin-900.19b7a0ad.woff2"
   },
   {
-    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
-    "url": "/pidro-counter/static/media/roboto-latin-100italic.6232f43d.woff2"
-  },
-  {
     "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
     "url": "/pidro-counter/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/pidro-counter/static/media/roboto-latin-100italic.6232f43d.woff2"
   },
   {
     "revision": "d704bb3d579b7d5e40880c75705c8a71",
@@ -60,20 +60,20 @@ self.__precacheManifest = [
     "url": "/pidro-counter/static/media/roboto-latin-300italic.d69924b9.woff2"
   },
   {
-    "revision": "210a7c781f5a354a0e4985656ab456d9",
-    "url": "/pidro-counter/static/media/roboto-latin-300italic.210a7c78.woff"
-  },
-  {
-    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
-    "url": "/pidro-counter/static/media/roboto-latin-400.bafb105b.woff"
-  },
-  {
     "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
     "url": "/pidro-counter/static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
     "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
     "url": "/pidro-counter/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/pidro-counter/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/pidro-counter/static/media/roboto-latin-300italic.210a7c78.woff"
   },
   {
     "revision": "de8b7431b74642e830af4d4f4b513ec9",
@@ -88,31 +88,31 @@ self.__precacheManifest = [
     "url": "/pidro-counter/static/media/roboto-latin-500italic.ffcc050b.woff"
   },
   {
-    "revision": "cf6613d1adf490972c557a8e318e0868",
-    "url": "/pidro-counter/static/media/roboto-latin-700.cf6613d1.woff"
-  },
-  {
     "revision": "010c1aeee3c6d1cbb1d5761d80353823",
     "url": "/pidro-counter/static/media/roboto-latin-700italic.010c1aee.woff2"
   },
   {
-    "revision": "846d1890aee87fde5d8ced8eba360c3a",
-    "url": "/pidro-counter/static/media/roboto-latin-700italic.846d1890.woff"
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/pidro-counter/static/media/roboto-latin-700.cf6613d1.woff"
   },
   {
     "revision": "8c2ade503b34e31430d6c98aa29a52a3",
     "url": "/pidro-counter/static/media/roboto-latin-900.8c2ade50.woff"
   },
   {
-    "revision": "bc833e725c137257c2c42a789845d82f",
-    "url": "/pidro-counter/static/media/roboto-latin-900italic.bc833e72.woff"
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/pidro-counter/static/media/roboto-latin-700italic.846d1890.woff"
   },
   {
     "revision": "7b770d6c53423deb1a8e49d3c9175184",
     "url": "/pidro-counter/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
   {
-    "revision": "d33b5920e46a72f339131eba891e66fa",
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/pidro-counter/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "e062512861fe9e8f90026b05b5fc3c1d",
     "url": "/pidro-counter/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pidro-counter/precache-manifest.64961ee966e44a8d7244eeaec6233ac3.js"
+  "/pidro-counter/precache-manifest.dad04d1bf6e9e5502bd8a1e344474fdb.js"
 );
 
 workbox.clientsClaim();
