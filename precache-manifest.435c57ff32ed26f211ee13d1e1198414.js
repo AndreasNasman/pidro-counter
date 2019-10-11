79dd3535cf71ecb23863f6b72bdf0b3b9ff14dc4
Andreas Näsman <andreas.nasman92@gmail.com>
2019-10-11 14:35:28 +0300

    Updates

---
+++ precache-manifest.435c57ff32ed26f211ee13d1e1198414.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d8491973711494a22005112f2e6f76",
+    "url": "/pidro-counter/index.html"
+  },
+  {
+    "revision": "3093bbd246c8e3f27d52",
+    "url": "/pidro-counter/static/css/2.b6e63b81.chunk.css"
+  },
+  {
+    "revision": "eba206868b85125d597b",
+    "url": "/pidro-counter/static/css/main.f77b6ee0.chunk.css"
+  },
+  {
+    "revision": "3093bbd246c8e3f27d52",
+    "url": "/pidro-counter/static/js/2.83a010ba.chunk.js"
+  },
+  {
+    "revision": "eba206868b85125d597b",
+    "url": "/pidro-counter/static/js/main.c9b279f6.chunk.js"
+  },
+  {
+    "revision": "ca5b43facb73b5003516",
+    "url": "/pidro-counter/static/js/runtime-main.994103b1.js"
+  },
+  {
+    "revision": "41568fe7927570e049a6cb707930f353",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-200.41568fe7.woff2"
+  },
+  {
+    "revision": "568dcb298a36f14317fdcddf8eded62f",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-200.568dcb29.woff"
+  },
+  {
+    "revision": "248b2236ba6bb9d2745a4908bc9c4568",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-300.248b2236.woff2"
+  },
+  {
+    "revision": "43fcfcf552695ff0976d7d03a29835c4",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-300.43fcfcf5.woff"
+  },
+  {
+    "revision": "1c37ab07cfefc70a85445f49737ad28a",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-400.1c37ab07.woff2"
+  },
+  {
+    "revision": "a4a08973559a45aba173184ea065addf",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-400.a4a08973.woff"
+  },
+  {
+    "revision": "67f6c1995acae33eacb1af1807c0e8c8",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-500.67f6c199.woff2"
+  },
+  {
+    "revision": "a39fc2de145c00ef5898c77b15c27517",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-500.a39fc2de.woff"
+  },
+  {
+    "revision": "bf5d453754c3bd3e0fc9bcdc979048f1",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-600.bf5d4537.woff2"
+  },
+  {
+    "revision": "c021e440e993ff26de945242222960a6",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-600.c021e440.woff"
+  },
+  {
+    "revision": "b46da14cbd07b2f1f5ca74069d59318e",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-700.b46da14c.woff2"
+  },
+  {
+    "revision": "c86a17b160684a125455d36f4733ad85",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-700.c86a17b1.woff"
+  },
+  {
+    "revision": "b0d773d996cad7ea914d35d16dd0f3ec",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-900.b0d773d9.woff"
+  },
+  {
+    "revision": "e985b6f17d53c2780c3c49722984a52e",
+    "url": "/pidro-counter/static/media/source-code-pro-latin-900.e985b6f1.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pidro-counter/precache-manifest.f40bace9d5c999f4593cf12c5a6e7e9a.js"
+  "/pidro-counter/precache-manifest.435c57ff32ed26f211ee13d1e1198414.js"
 );
 
 self.addEventListener('message', (event) => {
