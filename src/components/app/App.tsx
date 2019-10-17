1842ecf591dedcfe890316bddc9013ee32ec0680
Andreas Näsman <andreas.nasman92@gmail.com>
2019-10-17 16:06:03 +0300

    Changes font to Roboto and adds app level grid

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,7 +5,9 @@ import styles from "./App.module.css";
 export const App: React.FC = () => {
   return (
     <div className={styles.felt}>
-      <Keypad />
+      <div className={styles.grid}>
+        <Keypad />
+      </div>
     </div>
   );
 };
