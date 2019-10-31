d07799a579b6fb150dbfc1de79c47b0cbba4cf5e
Andreas Näsman <andreas.nasman92@gmail.com>
2019-10-31 10:25:58 +0200

    Changes reducer ordering

--- src/components/keypad/reducer.ts
+++ src/components/keypad/reducer.ts
@@ -7,10 +7,10 @@ interface State {
 }
 
 type Action =
-  | { team: Team | null; type: "SET_ACTIVE_TEAM" }
-  | { number: number | null; type: "SET_ACTIVE_NUMBER" }
   | { type: "DISABLE_BUTTONS" }
-  | { type: "RESET" };
+  | { type: "RESET" }
+  | { number: number | null; type: "SET_ACTIVE_NUMBER" }
+  | { team: Team | null; type: "SET_ACTIVE_TEAM" };
 
 export const initialState: State = {
   activeNumber: null,
@@ -20,14 +20,14 @@ export const initialState: State = {
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_ACTIVE_TEAM":
-      return { ...state, activeTeam: action.team };
-    case "SET_ACTIVE_NUMBER":
-      return { ...state, activeNumber: action.number };
     case "DISABLE_BUTTONS":
       return { ...state, disableButtons: false };
     case "RESET":
       return initialState;
+    case "SET_ACTIVE_NUMBER":
+      return { ...state, activeNumber: action.number };
+    case "SET_ACTIVE_TEAM":
+      return { ...state, activeTeam: action.team };
     default:
       throw new Error();
   }
