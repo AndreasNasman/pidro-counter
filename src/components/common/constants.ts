8f01a9544281509d61987d06aa9b88a6be190b2b
Andreas Näsman <andreas.nasman92@gmail.com>
2019-11-04 15:13:35 +0200

    Adds popover confirmation to reset

--- src/components/common/constants.ts
+++ src/components/common/constants.ts
@@ -3,3 +3,5 @@ import { Team } from "./types";
 export const MAXIMUM_POINTS = 14;
 export const MINIMUM_POINTS = 6;
 export const TEAMS: Team[] = ["us", "they"];
+
+export const MILLISECOND_MAGNITUDE = 1000;
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,6 +1,7 @@
 import { MdRedo, MdReplay, MdUndo } from "react-icons/md";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Icon } from "./icon";
+import { Popover } from "./popover";
 import { Props } from "./types";
 import styles from "./Toolbar.module.css";
 
@@ -11,10 +12,24 @@ export const Toolbar: FC<Props> = ({
   redo,
   reset,
   undo
-}) => (
-  <div className={styles.container}>
-    <Icon disabled={!canUndo} handleClick={undo} IconType={MdUndo} />
-    <Icon disabled={!canReset} handleClick={reset} IconType={MdReplay} />
-    <Icon disabled={!canRedo} handleClick={redo} IconType={MdRedo} />
-  </div>
-);
+}) => {
+  const [resetPopover, setResetPopover] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <Icon disabled={!canUndo} handleClick={undo} IconType={MdUndo} />
+      <Popover
+        affirmative={reset}
+        close={(): void => setResetPopover(false)}
+        isOpen={resetPopover}
+      >
+        <Icon
+          disabled={!canReset}
+          handleClick={(): void => setResetPopover(true)}
+          IconType={MdReplay}
+        />
+      </Popover>
+      <Icon disabled={!canRedo} handleClick={redo} IconType={MdRedo} />
+    </div>
+  );
+};
