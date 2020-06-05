11814397fc056aa2442b42ba2773a8162ad377db
Andreas Näsman <andreas.nasman92@gmail.com>
2020-06-05 10:10:08 +0300

    Moves files (#98)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import App from "components/app/App";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
