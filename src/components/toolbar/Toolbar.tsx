acef80e0baa8d1b2a089a22f46471989c2445d9d
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-11 20:44:59 +0300

    Implements icons

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,14 +1,37 @@
 import React, { FC } from "react";
 import { MdRedo, MdReplay, MdUndo } from "react-icons/md";
+import { Icon } from "./icon/Icon";
 import styles from "./Toolbar.module.css";
 
-// Disabled, onclicks
 export const Toolbar: FC = () => {
   return (
     <div className={styles.container}>
-      <MdUndo />
-      <MdReplay />
-      <MdRedo />
+      <Icon
+        disabled={false}
+        handleClick={(): void => {
+          // eslint-disable-next-line no-console
+          console.log("Undo");
+        }}
+        IconType={MdUndo}
+      />
+
+      <Icon
+        disabled={false}
+        handleClick={(): void => {
+          // eslint-disable-next-line no-console
+          console.log("Replay");
+        }}
+        IconType={MdReplay}
+      />
+
+      <Icon
+        disabled={false}
+        handleClick={(): void => {
+          // eslint-disable-next-line no-console
+          console.log("Redo");
+        }}
+        IconType={MdRedo}
+      />
     </div>
   );
 };
