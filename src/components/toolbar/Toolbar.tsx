e1ee67bf5103b0d5f23c1aef61d2e79a34ab3b38
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-11 16:19:35 +0300

    Adds toolbar content

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,5 +1,14 @@
 import React, { FC } from "react";
+import { MdRedo, MdReplay, MdUndo } from "react-icons/md";
+import styles from "./Toolbar.module.css";
 
+// Disabled, onclicks
 export const Toolbar: FC = () => {
-  return <div>Toolbar</div>;
+  return (
+    <div className={styles.container}>
+      <MdUndo />
+      <MdReplay />
+      <MdRedo />
+    </div>
+  );
 };
