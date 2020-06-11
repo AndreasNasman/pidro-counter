7f4756ea48ee01182b7bf071991b7e0a71541d87
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-11 15:49:34 +0300

    Adds scoreboard content

---
+++ src/components/common/constants.ts
@@ -0,0 +1,7 @@
+import { Team } from "reducers/game/types";
+
+export const MAXIMUM_POINTS = 14;
+export const MINIMUM_POINTS = 6;
+export const TEAMS: Team[] = ["us", "they"];
+
+export const MILLISECOND_MAGNITUDE = 1000;
--- src/reducers/game/initialState.ts
+++ src/reducers/game/initialState.ts
@@ -2,4 +2,5 @@ import { State } from "./types";
 
 export const initialState: State = {
   game: [],
+  score: { they: 0, us: 0 },
 };
