e82163378237cbb5f2f7730164c258c6c5256caf
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-11 13:24:06 +0300

    Adds scoreboard

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,14 @@
+import { Scoreboard } from "components/scoreboard/Scoreboard";
 import React, { FC } from "react";
 import styles from "./App.module.css";
 
 const App: FC = () => {
   return (
     <div className={styles.felt}>
-      <div className={styles.grid}>game</div>
+      <div className={styles.grid}>
+        game
+        <Scoreboard />
+      </div>
     </div>
   );
 };
---
+++ src/components/scoreboard/Scoreboard.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import styles from "./Scoreboard.module.css";
+
+export const Scoreboard: FC = () => {
+  return <div className={styles.table}>Scoreboard</div>;
+};
