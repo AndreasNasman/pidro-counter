0327c9b3f6bbc4dff8b01670d87476f359271d9b
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-12 15:49:09 +0300

    Refactors constants

--- src/components/common/constants.ts
+++ src/components/common/constants.ts
@@ -3,6 +3,4 @@ import { Team } from "reducers/game/types";
 export const MAXIMUM_POINTS = 14;
 export const MINIMUM_POINTS = 6;
 
-export const MILLISECOND_MAGNITUDE = 1000;
-
 export const TEAMS: Team[] = ["us", "they"];
--- src/components/keypad/constants.ts
+++ src/components/keypad/constants.ts
@@ -1,8 +1,4 @@
-import {
-  MAXIMUM_POINTS,
-  MILLISECOND_MAGNITUDE,
-  MINIMUM_POINTS,
-} from "components/common/constants";
+import { MAXIMUM_POINTS, MINIMUM_POINTS } from "components/common/constants";
 import buttonStyles from "./button/Button.module.css";
 
 const INCLUSIVE_END_OFFSET = 1;
@@ -11,5 +7,6 @@ export const NUMBERS = Array.from(
   (_value: number, index: number) => index + MINIMUM_POINTS
 );
 
+export const MILLISECOND_MAGNITUDE = 1000;
 export const TIMEOUT =
   parseFloat(buttonStyles.duration) * MILLISECOND_MAGNITUDE;
