397d9d2e2462f3203f032c71ac11841ba8197d45
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-12 15:45:35 +0300

    Adds prompts

--- src/components/keypad/Keypad.tsx
+++ src/components/keypad/Keypad.tsx
@@ -5,6 +5,7 @@ import { Button } from "./button/Button";
 import buttonStyles from "./button/Button.module.css";
 import { NUMBERS } from "./constants";
 import styles from "./Keypad.module.css";
+import { Prompt } from "./prompt/Prompt";
 
 export const Keypad: FC = () => {
   const [activeTeam, setActiveTeam] = useState<Team | null>(null);
@@ -21,7 +22,7 @@ export const Keypad: FC = () => {
 
   return (
     <div className={styles.container}>
-      <h2>Phase what?</h2>
+      <Prompt />
 
       <div className={styles.team}>
         {TEAMS.map((team) => (
