4f2f97873e6fb2158dcad4c2ee44c3d5f61e4940
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-12 15:14:46 +0300

    Implements button

--- src/components/keypad/Keypad.tsx
+++ src/components/keypad/Keypad.tsx
@@ -1,27 +1,51 @@
 import { TEAMS } from "components/common/constants";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Team } from "reducers/game/types";
+import { Button } from "./button/Button";
+import buttonStyles from "./button/Button.module.css";
 import { NUMBERS } from "./constants";
 import styles from "./Keypad.module.css";
 
 export const Keypad: FC = () => {
+  const [activeTeam, setActiveTeam] = useState<Team | null>(null);
+  const [activeNumber, setActiveNumber] = useState<number | null>(null);
+  const [disableButtons] = useState(false);
+
+  const handleTeamClick = (team: Team): void => {
+    setActiveTeam(team);
+  };
+
+  const handleNumberClick = (number: number): void => {
+    setActiveNumber(number);
+  };
+
   return (
     <div className={styles.container}>
       <h2>Phase what?</h2>
 
-      <div className={styles.team_container}>
-        {TEAMS.map((team: Team) => (
-          <button className={styles.button} key={team}>
-            {team}
-          </button>
+      <div className={styles.team}>
+        {TEAMS.map((team) => (
+          <Button
+            active={team === activeTeam}
+            activeColor={buttonStyles.black}
+            disabled={disableButtons}
+            handleClick={handleTeamClick}
+            key={team}
+            value={team}
+          />
         ))}
       </div>
 
-      <div className={styles.digit_container}>
-        {NUMBERS.map((digit: number) => (
-          <button className={styles.button} key={digit}>
-            {digit}
-          </button>
+      <div className={styles.number}>
+        {NUMBERS.map((number) => (
+          <Button
+            active={number === activeNumber}
+            activeColor={buttonStyles.red}
+            disabled={disableButtons}
+            handleClick={handleNumberClick}
+            key={number}
+            value={number}
+          />
         ))}
       </div>
     </div>
