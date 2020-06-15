02e551d8b357b5d36074ac57534ab0ec6a64dbe9
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-06-15 15:05:20 +0300

    Cleans up body

--- src/components/scoreboard/body/Body.tsx
+++ src/components/scoreboard/body/Body.tsx
@@ -5,13 +5,12 @@ import React, { FC, useEffect, useRef } from "react";
 import styles from "../Scoreboard.module.css";
 
 export const Body: FC = () => {
-  const bodyColumnRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    // eslint-disable-next-line no-unused-expressions
-    bodyColumnRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
-  });
-
   const { game } = useGameContext().state;
+
+  const bodyColumnRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() =>
+    bodyColumnRef.current?.scrollIntoView({ behavior: "smooth", block: "end" })
+  );
 
   return (
     <div className={styles.body}>
@@ -20,14 +19,14 @@ export const Body: FC = () => {
           {game.map((round, index) => (
             <div
               className={classNames(styles.cell, {
-                [styles.reverse]: TEAMS[TEAMS.length - 1] === team,
+                [styles.reverse]: team === TEAMS[TEAMS.length - 1],
               })}
               key={index}
             >
-              {typeof round.score === "object" ? (
+              {round.score ? (
                 <div className={styles.content}>{round.score[team]}</div>
               ) : (
-                round.bid.team === team && (
+                team === round.bid.team && (
                   <>
                     <div className={styles.content}>{round.bid.points}</div>
                     <span
